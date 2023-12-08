60b6e27eae942f9ad203192db271c5f6a652762e
baoht42 <snorlax4293@gmail.com>
2023-12-08 08:35:29 +0700

    lam case 0-19

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to read: ");
+        int inputNum = sc.nextInt();
+        if (inputNum >= 0 && inputNum < 10) {
+            switch (inputNum) {
+                case 0:
+                    System.out.println("Zero");
+                    break;
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
+                default:
+                    System.out.println("Out off ability");
+            }
+            if (inputNum >= 10 && inputNum < 20) {
+                int ones = inputNum % 10;
+                switch (ones) {
+                    case 0:
+                        System.out.println("Ten");
+                        break;
+                    case 1:
+                        System.out.println("Eleven");
+                        break;
+                    case 2:
+                        System.out.println("Twelve");
+                        break;
+                    case 3:
+                        System.out.println("Thirteen");
+                        break;
+                    case 4:
+                        System.out.println("Four");
+                        break;
+                    case 5:
+                        System.out.println("Five");
+                        break;
+                    case 6:
+                        System.out.println("Six");
+                        break;
+                    case 7:
+                        System.out.println("Seven");
+                        break;
+                    case 8:
+                        System.out.println("Eight");
+                        break;
+                    case 9:
+                        System.out.println("Nine");
+                        break;
+                    default:
+                        System.out.println("Out of ability");
+                }
+            }
+        }
+    }
+}
